813e1aa43d3b8ee245a724ee96cdd5bfebe5bdf5
interrrp <watchyourcache@gmail.com>
2025-03-23 12:20:57 +0800

    Create board

---
+++ crosses/board.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+from enum import Enum
+
+
+class Mark(Enum):
+    NONE = "."
+    X = "X"
+    O = "O"  # noqa: E741
+
+
+class Outcome(Enum):
+    X_WON = "X won!"
+    O_WON = "O won!"
+    TIE = "Tie!"
+
+    @staticmethod
+    def from_mark(mark: Mark) -> Outcome:
+        return Outcome.X_WON if mark == Mark.X else Outcome.O_WON
+
+
+class InvalidUnmarkError(Exception):
+    pass
+
+
+class Board:
+    def __init__(self) -> None:
+        self._board = [Mark.NONE] * 9
+        self._turn = Mark.X
+
+    @property
+    def turn(self) -> Mark:
+        return self._turn
+
+    @property
+    def outcome(self) -> Outcome | None:
+        if not any(self.legal_indices):
+            return Outcome.TIE
+
+        for row in range(3):
+            if self[row, 0] != Mark.NONE and self[row, 0] == self[row, 1] == self[row, 2]:
+                return Outcome.from_mark(self[row, 0])
+
+        for col in range(3):
+            if self[0, col] != Mark.NONE and self[0, col] == self[1, col] == self[2, col]:
+                return Outcome.from_mark(self[0, col])
+
+        if self[0, 0] != Mark.NONE and self[0, 0] == self[1, 1] == self[2, 2]:
+            return Outcome.from_mark(self[0, 0])
+
+        if self[0, 2] != Mark.NONE and self[0, 2] == self[1, 1] == self[2, 0]:
+            return Outcome.from_mark(self[0, 2])
+
+        return None
+
+    @property
+    def legal_indices(self) -> list[int]:
+        indices: list[int] = []
+        for index, mark in enumerate(self._board):
+            if mark == Mark.NONE:
+                indices.append(index)
+        return indices
+
+    def __getitem__(self, index: int | tuple[int, int]) -> Mark:
+        if isinstance(index, tuple):
+            row = index[0]
+            col = index[1]
+            return self._board[row * 3 + col]
+        return self._board[index]
+
+    def mark(self, index: int) -> None:
+        self._board[index] = self._turn
+        self._invert_turn()
+
+    def unmark(self, index: int) -> None:
+        if self._board[index] == self._turn:
+            raise InvalidUnmarkError
+
+        self._board[index] = Mark.NONE
+        self._invert_turn()
+
+    def set_state(self, row_1: str, row_2: str, row_3: str) -> None:
+        all_rows = f"{row_1} {row_2} {row_3}".split()
+        for index, row in enumerate(all_rows):
+            self._board[index] = Mark(row)
+
+    def _invert_turn(self) -> None:
+        if self._turn == Mark.X:
+            self._turn = Mark.O
+        elif self._turn == Mark.O:
+            self._turn = Mark.X
---
+++ tests/test_board.py
@@ -0,0 +1,62 @@
+import pytest
+
+from crosses.board import Board, InvalidUnmarkError, Mark, Outcome
+
+
+def test_board_is_initialized_empty() -> None:
+    board = Board()
+    for index in range(9):
+        assert board[index] == Mark.NONE
+
+
+def test_board_marks_and_unmarks_squares() -> None:
+    board = Board()
+    assert board[0] == Mark.NONE
+    assert board.turn == Mark.X
+    board.mark(0)
+    assert board[0] == Mark.X
+    assert board.turn == Mark.O
+    board.unmark(0)
+    assert board[0] == Mark.NONE
+    assert board.turn == Mark.X
+
+
+def test_board_invalid_unmark() -> None:
+    board = Board()
+    board.mark(0)
+    board.mark(1)
+    with pytest.raises(InvalidUnmarkError):
+        board.unmark(0)
+
+
+def test_board_determines_no_outcome() -> None:
+    board = Board()
+    assert board.outcome is None
+
+
+def test_board_determines_winner() -> None:
+    board = Board()
+
+    board.set_state(
+        "X . .",
+        ". X .",
+        ". . X",
+    )
+    assert board.outcome == Outcome.X_WON
+
+    board.set_state(
+        ". . O",
+        ". O .",
+        "O . .",
+    )
+    assert board.outcome == Outcome.O_WON
+
+
+def test_board_determines_tie() -> None:
+    board = Board()
+    board.set_state(
+        "X O X",
+        "O X O",
+        "X O X",
+    )
+    assert board.outcome == Outcome.TIE
